bb3d06fb1fd8aeaa062ae1e32512c8ed6b8034da
mingjingc <2595400537@qq.com>
2020-11-06 23:21:35 +0800

    add GetSingers GetVote GetCandidateVote GetSingers

--- consensus/dpos/api.go
+++ consensus/dpos/api.go
@@ -1,6 +1,9 @@
 package dpos
 
 import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -22,4 +25,56 @@ func (api *API) GetSnapshot(number *rpc.BlockNumber) (*Snapshot, error) {
 		return nil, errUnknownBlock
 	}
 	return api.dpos.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
+}
+
+func (api *API) GetCandidateVote(number *rpc.BlockNumber, candidate common.Address) (*big.Int, error) {
+	var header *types.Header
+	if number == nil || *number == rpc.LatestBlockNumber {
+		header = api.chain.CurrentHeader()
+	} else {
+		header = api.chain.GetHeaderByNumber(uint64(number.Int64()))
+	}
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	snp, err := api.dpos.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return snp.CandidateTally[candidate], nil
+}
+
+func (api *API) GetVote(number *rpc.BlockNumber, candidate common.Address) (*Vote, error) {
+	var header *types.Header
+	if number == nil || *number == rpc.LatestBlockNumber {
+		header = api.chain.CurrentHeader()
+	} else {
+		header = api.chain.GetHeaderByNumber(uint64(number.Int64()))
+	}
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	snp, err := api.dpos.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return snp.Votes[candidate], nil
+}
+
+func (api *API) GetSingers(number *rpc.BlockNumber) ([]common.Address, error) {
+	var header *types.Header
+	if number == nil || *number == rpc.LatestBlockNumber {
+		header = api.chain.CurrentHeader()
+	} else {
+		header = api.chain.GetHeaderByNumber(uint64(number.Int64()))
+	}
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	snp, err := api.dpos.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return snp.signers(), nil
 }
